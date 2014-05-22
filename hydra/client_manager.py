cfb8f6d526f158951fa90a847aa57cf3c66b26cc
manno23 <jasonmanning23@gmial.com>
2014-05-22 14:41:26 +0800

    Moved midi event detection up into the event loop.

--- hydra/client_manager.py
+++ hydra/client_manager.py
@@ -92,7 +92,7 @@ class ClientManager(object):
         should handle cases when there are no clients to become active for example
         """
         events = self.midi_handler.poll_midi_events()
-        if len(self.client_list) > 0:  # If there are clients available to send to
+        if events is not None and len(self.client_list) > 0:  # If there are clients available to send to
 
             for handle_type, message, channel in events:
                 if handle_type is not None:
